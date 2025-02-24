db673f17bc5afd978ce2cad0b7a3d51e5194d5c6
meena_developer <meenaweb1997@gmail.com>
2025-02-24 17:43:17 +0530

    package install

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,8 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
-
+import Mui from "./Mui";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 function App() {
   const [count, setCount] = useState(0);
 
@@ -63,52 +64,11 @@ function App() {
 
   return (
     <>
-     
-      
-      
-      <ul role="list" className="divide-y divide-gray-100">
-        {people.map((person) => (
-          <li key={person.email} className="flex justify-between gap-x-6 py-5">
-            <div className="flex min-w-0 gap-x-4">
-              <img
-                alt=""
-                src={person.imageUrl}
-                className="size-12 flex-none rounded-full bg-gray-50"
-              />
-              <div className="min-w-0 flex-auto">
-                <p className="text-sm/6 font-semibold text-gray-900">
-                  {person.name}
-                </p>
-                <p className="mt-1 truncate text-xs/5 text-gray-500">
-                  {person.email}
-                </p>
-              </div>
-            </div>
-            <div className="hidden shrink-0 sm:flex sm:flex-col sm:items-end">
-              <p className="text-sm/6 text-gray-900">{person.role}</p>
-              {person.lastSeen ? (
-                <p className="mt-1 text-xs/5 text-gray-500">
-                  Last seen{" "}
-                  <time dateTime={person.lastSeenDateTime}>
-                    {person.lastSeen}
-                  </time>
-                </p>
-              ) : (
-                <div className="mt-1 flex items-center gap-x-1.5">
-                  <div className="flex-none rounded-full bg-emerald-500/20 p-1">
-                    <div className="size-1.5 rounded-full bg-emerald-500" />
-                  </div>
-                  <p className="text-xs/5 text-gray-500">Online</p>
-                </div>
-              )}
-            </div>
-          </li>
-        ))}
-      </ul>
-
-      <h1 class="text-3xl bg-red-700 font-400 ">
-        React 19 with Tailwind CSS v4
-      </h1>
+      <Router>
+        <Routes>
+          <Route path="/mui" element={<Mui />} />
+        </Routes>
+      </Router>
     </>
   );
 }
---
+++ src/Mui.jsx
@@ -0,0 +1,97 @@
+import Box from "@mui/material/Box";
+import { DataGrid } from "@mui/x-data-grid";
+import { motion } from "framer-motion";
+import axios from "axios";
+import { useEffect } from "react";
+import MegaMenu from "./MegaMenu";
+const Mui = () => {
+  const columns = [
+    { field: "id", headerName: "ID", width: 90 },
+    {
+      field: "firstName",
+      headerName: "First name",
+      width: 150,
+      editable: true,
+    },
+    {
+      field: "lastName",
+      headerName: "Last name",
+      width: 150,
+      editable: true,
+    },
+    {
+      field: "age",
+      headerName: "Age",
+      type: "number",
+      width: 110,
+      editable: true,
+    },
+    {
+      field: "fullName",
+      headerName: "Full name",
+      description: "This column has a value getter and is not sortable.",
+      sortable: false,
+      width: 160,
+      valueGetter: (value, row) =>
+        `${row.firstName || ""} ${row.lastName || ""}`,
+    },
+  ];
+
+  const rows = [
+    { id: 1, lastName: "Snow", firstName: "Jon", age: 14 },
+    { id: 2, lastName: "Lannister", firstName: "Cersei", age: 31 },
+    { id: 3, lastName: "Lannister", firstName: "Jaime", age: 31 },
+    { id: 4, lastName: "Stark", firstName: "Arya", age: 11 },
+    { id: 5, lastName: "Targaryen", firstName: "Daenerys", age: null },
+    { id: 6, lastName: "Melisandre", firstName: null, age: 150 },
+    { id: 7, lastName: "Clifford", firstName: "Ferrara", age: 44 },
+    { id: 8, lastName: "Frances", firstName: "Rossini", age: 36 },
+    { id: 9, lastName: "Roxie", firstName: "Harvey", age: 65 },
+  ];
+
+  const fetchData = async () => {
+    try {
+      const response = await axios.get(
+        "https://www.epiic.amrithaa.net/backend/api/get-states"
+      );
+      console.log("axios check", response.data);
+    } catch (error) {
+      console.error("error");
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  return (
+    <>
+      <MegaMenu />
+      <motion.div
+        animate={{ scale: 1.2 }}
+        transition={{ duration: 0.5 }}
+        style={{ maxWidth: "200px" }}
+      >
+        Hover over me!
+      </motion.div>
+      <Box sx={{ height: 400, width: "100%" }}>
+        <DataGrid
+          rows={rows}
+          columns={columns}
+          initialState={{
+            pagination: {
+              paginationModel: {
+                pageSize: 5,
+              },
+            },
+          }}
+          pageSizeOptions={[5]}
+          checkboxSelection
+          disableRowSelectionOnClick
+        />
+      </Box>
+    </>
+  );
+};
+
+export default Mui;
